128f9a03d056ab0458f074f445e0f5adbebaadd6
4everlad <d.s.bakulin@gmail.com>
2023-01-23 13:55:02 +0300

    update

--- Autodoc/NewsFeed/ViewModel/NewsFeedViewModel.swift
+++ Autodoc/NewsFeed/ViewModel/NewsFeedViewModel.swift
@@ -30,40 +30,38 @@ class NewsFeedViewModel: ObservableObject {
     
     func getNews(completion: (() -> Void)? = nil) {
         
-        guard newsFeed.count <= pagination.totalItems else { return }
-        guard canLoad == true else { return }
+        guard newsFeed.count <= pagination.totalItems else {
+            completion?()
+            return }
+        guard canLoad == true else {
+            completion?()
+            return }
         
         Task() {
-            do {
-                
-                self.canLoad = false
-                
-                let result = await networkClient.getNewsFeedAsync(pageCount: pagination.page, newsCount: pagination.itemsCount)
-                
-                guard let feed = result else {
-                    self.canLoad = true
-                    completion?()
-                    return
-                }
-                
-                let newsItems: [NewsItem] = feed.news.compactMap {
-                    let survey = NewsItem(with: $0)
-                    return survey
-                }
-                
-                DispatchQueue.main.async {
-                    self.newsFeed.append(contentsOf: newsItems)
-                }
-                
-                pagination.page += 1
-                pagination.totalItems = feed.totalCount
+            
+            self.canLoad = false
+            
+            let result = await networkClient.getNewsFeedAsync(pageCount: pagination.page, newsCount: pagination.itemsCount)
+            
+            guard let feed = result else {
                 self.canLoad = true
                 completion?()
-                
-            } catch {
-                print("async get news error")
-                completion?()
+                return
             }
+            
+            let newsItems: [NewsItem] = feed.news.compactMap {
+                let survey = NewsItem(with: $0)
+                return survey
+            }
+            
+            DispatchQueue.main.async {
+                self.newsFeed.append(contentsOf: newsItems)
+            }
+            
+            pagination.page += 1
+            pagination.totalItems = feed.totalCount
+            self.canLoad = true
+            completion?()
         }
         
     }
